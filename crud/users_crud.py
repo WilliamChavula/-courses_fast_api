044ec35cedbfee736c141acaabb75c59d3abfea1
williamchavula <rumbani@gmail.com>
2022-08-29 20:15:39 -0500

    minor refactors

--- crud/users_crud.py
+++ crud/users_crud.py
@@ -10,36 +10,7 @@ from schemas.user_schemas import UserCreate
 # CREATE QUERIES #
 
 
-async def db_create_user(db: Session, user: UserCreate) -> UserModel:
-    user_ = UserModel(
-        id=user.id,
-        first_name=user.first_name,
-        last_name=user.last_name,
-        email=user.email,
-        password=get_password_hash(user.password),
-        job_title=user.job_title,
-        is_super_user=user.is_super_user,
-    )
-
-    db.add(user_)
-    db.commit()
-    return user_
-
-
-async def db_insert_many(db: Session, users: List[UserCreate]) -> List[UserModel]:
-    users_coro = [
-        _create_many_helper(user)
-        for user in users
-    ]
-
-    users_ = await asyncio.gather(*users_coro, return_exceptions=True)
-
-    db.add_all(users_)
-    db.commit()
-    return list(users_)
-
-
-async def _create_many_helper(user: UserCreate) -> UserModel:
+async def _create_user_helper(user: UserCreate) -> UserModel:
     return UserModel(
         id=user.id,
         first_name=user.first_name,
@@ -51,19 +22,31 @@ async def _create_many_helper(user: UserCreate) -> UserModel:
     )
 
 
+async def db_create_user(db: Session, user: UserCreate) -> UserModel:
+    user_ = await _create_user_helper(user)
+    db.add(user_)
+    db.commit()
+
+    return user_
+
+
+async def db_insert_many(db: Session, users: List[UserCreate]) -> List[UserModel]:
+    users_coro = [_create_user_helper(user) for user in users]
+
+    users_ = await asyncio.gather(*users_coro, return_exceptions=True)
+
+    db.add_all(users_)
+    db.commit()
+    return list(users_)
+
+
 async def user_registration(request: UserCreate, database) -> UserModel:
-    new_user = UserModel(
-        id=request.id,
-        first_name=request.first_name,
-        last_name=request.last_name,
-        email=request.email,
-        password=get_password_hash(request.password),
-        is_super_user=request.is_super_user,
-        job_title=request.job_title,
-    )
+    new_user = await _create_user_helper(user=request)
+
     database.add(new_user)
     database.commit()
     database.refresh(new_user)
+
     return new_user
 
 
